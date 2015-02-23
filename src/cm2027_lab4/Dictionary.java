4eed58c42328ad84bf29b5e615dc0ccfb3fe2a66
1204798 <1204798@RG43011.rgu.ac.uk>
2015-02-23 09:53:01 +0000

    First commit
    Added:
    	- main class
    	- Dictionary interface

---
+++ src/cm2027_lab4/Dictionary.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cm2027_lab4;
+
+/**
+ *
+ * @author roryleng (1204798)
+ */
+public interface Dictionary {
+
+	/**
+	 *
+	 * @param input The word to be defined
+	 * @return The definition of the word
+	 */
+	public String getDefinition(String input);
+}
